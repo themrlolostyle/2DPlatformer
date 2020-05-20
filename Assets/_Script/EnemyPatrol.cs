2ed0b96f2eb0551ab66927215342d68501f3b234
Vasyl Yevchyn <vasagaminginc@gmail.com>
2020-05-20 12:48:53 +0200

    Add spawn coin, add enemys

---
+++ Assets/_Script/EnemyPatrol.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] Transform[] _points;
+    [SerializeField] Transform _enemy;
+    [SerializeField] float _speed;
+
+    private int currendPoint;
+
+    private void Update()
+    {
+        Transform target = _points[currendPoint];
+
+        _enemy.position = Vector3.MoveTowards(_enemy.transform.position, target.position, _speed * Time.deltaTime);
+
+        if (_enemy.position == target.position)
+        {
+            currendPoint++;
+            if (currendPoint >= _points.Length)
+            {
+                currendPoint = 0;
+            }
+        }
+        
+    }
+}
